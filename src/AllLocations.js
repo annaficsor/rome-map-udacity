d1665c4a980066c4812832dff25764a4a95265ac
Anna Ficsor <ficsor.anna@gmail.com>
2018-06-29 13:47:42 +0200

    style: Improve readibility

--- src/AllLocations.js
+++ src/AllLocations.js
@@ -20,7 +20,6 @@ class AllLocations extends Component {
     this.props.onMap(marker)
   }
 
-
   render() {
 
     return (
@@ -30,46 +29,45 @@ class AllLocations extends Component {
           onUpdateType = {this.props.onUpdateType}
         />
 
-        {(this.props.selectedType.length < 1 || this.props.selectedType === 'All') &&(
-        <div className="list">
-        {this.state.listNames.map((title, index) => (
-          <div key={title[index]} className='listChild'>
-          <h2 className={`listType ${title}`}>{title}</h2>
-            <div className="list-items">
-              <ul className="list-grid">
-              {this.props.markers
-                .filter(type => type.id === title)
-                .map(location => (
-                <li className={`loc${location.id}`} key={location.title} onClick={(e) => this.handleClick(location)}>{location.title}</li>))
-              }
-              </ul>
+        {(this.props.selectedType.length < 1 || this.props.selectedType === 'All') && (
+          <div className="list">
+            {this.state.listNames.map((title, index) => (
+              <div key={title[index]} className='listChild'>
+                <h2 className={`listType ${title}`}>{title}</h2>
+                <div className="list-items">
+                  <ul className="list-grid">
+                    {this.props.markers
+                      .filter(type => type.id === title)
+                      .map(location => (
+                      <li className={`loc${location.id}`} key={location.title} onClick={(e) => this.handleClick(location)}>{location.title}</li>))
+                    }
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         )}
 
         {this.props.selectedType.length > 1 && (
-        <div className="list">
-        {this.state.listNames
-          .filter(type => type === this.props.selectedType)
-          .map((title, index) => (
-          <div key={title[index]} className='listChild'>
-          <h2 className={`listType ${title}`}>{title}</h2>
-            <div className="list-items">
-              <ul className="list-grid">
-              {this.props.markers
-                .filter(type => type.id === title)
-                .map(location => (
-                <li className={`loc${location.id}`} key={location.title} onClick={(e) => this.handleClick(location)}>{location.title}</li>))
-              }
-              </ul>
+          <div className="list">
+            {this.state.listNames
+              .filter(type => type === this.props.selectedType)
+              .map((title, index) => (
+              <div key={title[index]} className='listChild'>
+                <h2 className={`listType ${title}`}>{title}</h2>
+                <div className="list-items">
+                  <ul className="list-grid">
+                    {this.props.markers
+                      .filter(type => type.id === title)
+                      .map(location => (
+                      <li className={`loc${location.id}`} key={location.title} onClick={(e) => this.handleClick(location)}>{location.title}</li>))
+                    }
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         )}
-
       </div>
     )
   }
