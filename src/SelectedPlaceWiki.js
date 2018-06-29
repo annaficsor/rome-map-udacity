0fc86cda9a5d4f3de93f26c6e7759c7c068215c1
Anna Ficsor <ficsor.anna@gmail.com>
2018-06-29 13:59:12 +0200

    style: Improve readibility

--- src/SelectedPlaceWiki.js
+++ src/SelectedPlaceWiki.js
@@ -19,21 +19,23 @@ class SelectedPlaceWiki extends Component {
   render() {
     return (
       <div className="place">
-      {this.props.wikiName.length < 1 && (
-        <div className="holder">
-          <div className="preloader"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
-      )}
+        {this.props.wikiName.length < 1 && (
+          <div className="holder">
+            <div className="preloader"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          </div>
+        )}
 
-      {this.props.wikiName.length > 1 && (
-      <div>
-      <div className="back" onClick={() => this.handleClick()} >
-      <img src={arrow} alt="Go back arrow"/><span className="backtext">BACK</span>
-      </div>
-          <h1>{this.props.wikiName}</h1>
-          <div className="wikitext">{this.props.wiki}</div>
-          <div className="link"><a href={this.props.wikiUrl}>More information >></a></div>
-          <img className="placeimage" src={this.props.wikiImage} alt={this.props.wikiName}/>
+        {this.props.wikiName.length > 1 && (
+          <div>
+            <div className="back" onClick={() => this.handleClick()} >
+              <img src={arrow} alt="Go back arrow"/><span className="backtext">BACK</span>
+            </div>
+            <h1>{this.props.wikiName}</h1>
+            <div className="wikitext">{this.props.wiki}</div>
+            <div className="link">
+              <a href={this.props.wikiUrl}>More information >></a>
+            </div>
+            <img className="placeimage" src={this.props.wikiImage} alt={this.props.wikiName}/>
           </div>
         )}
       </div>
